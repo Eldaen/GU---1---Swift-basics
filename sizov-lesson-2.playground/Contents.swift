da256b241f8aa380d27e91531a2c240ef1c56672
Sizov Denis <sv_skatch@bk.ru>
2021-08-27 23:54:44 +0300

    Task 5 is done

--- sizov-lesson-2.playground/Contents.swift
+++ sizov-lesson-2.playground/Contents.swift
@@ -42,6 +42,7 @@ var arrayCopy2 = x
  Задание 4
  
  Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+ 
  Наделал аж 3 варианта, т.к. с первого наскока не смог сделать через for in цикл и remove, в итоге сделал через while цикл и ещё вариантов
  */
 
@@ -111,4 +112,38 @@ while loopCounter < arrayCopy2.count {
     }
 }
 
-print(arrayCopy2)
+print(arrayCopy2, "\n\n\n")
+
+
+/*
+ Задание 5
+ 
+ * Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов.
+ 
+ В задании нет подробностей, что там за массив итд, придётся делать универсального солдата. Даже если в массиве какие-то странные числа, то пусть считает сумму двух последних и дальше от этого пляшет.
+ */
+
+func addFibonacci (_ array: inout [Int], howMany count: Int) {
+    switch array.count {
+    case 0:
+        array += [0, 1] // если пришёл пустой массив, то начнём как нормальный ряд Фибоначчи
+    case 1:
+        array.append(array[0]) // если элемент в массиве один, то предыдущий равен 0.
+    default:
+        break
+    }
+    
+    for _ in 0..<count {
+        let newVal = array[array.endIndex - 2] + array[array.endIndex-1] // в 2 строчки как-то красивее, чем сразу аппендить. Внезапно узнал, что endIndex почему-то на 1 больше, чем реальный конец О_о
+        array.append(newVal)
+    }
+}
+
+var fibonacci: [Int] = []
+addFibonacci(&fibonacci, howMany: 50)
+print(fibonacci, "\n\n\n")
+
+var fibonacci2: [Int] = [10, 31]
+addFibonacci(&fibonacci2, howMany: 50)
+print(fibonacci2, "\n\n\n")
+
