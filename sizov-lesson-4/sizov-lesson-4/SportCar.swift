6ef020ebc9c631b147fe202033b60d9a6035f5bf
Sizov Denis <sv_skatch@bk.ru>
2021-09-07 13:15:46 +0300

    SportCar class is done

--- sizov-lesson-4/sizov-lesson-4/SportCar.swift
+++ sizov-lesson-4/sizov-lesson-4/SportCar.swift
@@ -12,14 +12,28 @@ class SportCar: Car {
     var aeroSpoiler: Bool
     let panoramicRoof: Bool
     var passangersCount: Int
+    var aeroSpoilerState: spoilerState
     
-    // TODO: Добавить доп. варианты действий для doSomething
+    override func doSomething(whatToDo: carActions) {
+        super.doSomething(whatToDo: whatToDo)
+        
+        switch whatToDo {
+            case .raiseTheSpoiler:
+                self.aeroSpoilerState = .risen
+            case .lowerTheSpoiler:
+                self.aeroSpoilerState = .lowered
+        default:
+            break
+        }
+    }
     
     init (brand: String, dateOfCreation: Date, trunkVolume: Int, panoramicRoof: Bool, aeroSpoiler: Bool, passangersCount: Int) {
         
         self.panoramicRoof = panoramicRoof
         self.aeroSpoiler = aeroSpoiler
         self.passangersCount = passangersCount
+        
+        aeroSpoilerState = .lowered
         
         super.init(brand: brand, dateOfCreation: dateOfCreation, trunkVolume: trunkVolume)
     }
