896d22842a6bab35e75bb76a9f81b2f7cf66fc37
Sizov Denis <sv_skatch@bk.ru>
2021-09-07 13:16:03 +0300

    TrunkCar class is done

--- sizov-lesson-4/sizov-lesson-4/TrunkCar.swift
+++ sizov-lesson-4/sizov-lesson-4/TrunkCar.swift
@@ -12,15 +12,46 @@ class TrunkCar: Car {
     var maxWeight: Int
     var sleepingSpot: Bool
     var gasTankVolume: Int
+    var trunkBackCopacity: Int
+    var copacityRemaining: Int {
+        get { return self.copacityRemaining }
+        set {
+            let newCopacityRemaining = trunkBackCopacity - newValue
+            if newCopacityRemaining > 0 { // надо убедиться, что не грузят через край
+                self.copacityRemaining = newCopacityRemaining
+            }
+        }
+    }
+    var storedCargo: Int = 0
+
     
-    // TODO: Добавить доп. варианты действий для doSomething
+    override func doSomething(whatToDo: carActions) {
+        super.doSomething(whatToDo: whatToDo)
+            
+        if whatToDo == .emptyTheBack {
+            self.storedCargo = 0
+        }
+        }
     
-    init (brand: String, dateOfCreation: Date, trunkVolume: Int, maxWeight: Int, sleepingSpot: Bool, gasTankVolume: Int) {
+    func addCargo (howMuch newCargo: Int) {
+        let newCargoWeight = self.storedCargo + newCargo
+        if newCargoWeight < self.trunkBackCopacity { // если сумму текущего груза + новый не превышает вместимость, то можно положить
+            self.storedCargo += newCargo
+            print("\(newCargo) of cargo weight were added to the trunk")
+        } else {
+            print("Can't carry so much weight")
+        }
+    }
+    
+    
+    init(brand: String, dateOfCreation: Date, trunkVolume: Int, maxWeight: Int, sleepingSpot: Bool, gasTankVolume: Int, trunkBackCopacity: Int) {
         
         self.maxWeight = maxWeight
         self.sleepingSpot = sleepingSpot
         self.gasTankVolume = gasTankVolume
+        self.trunkBackCopacity = trunkBackCopacity
         
         super.init(brand: brand, dateOfCreation: dateOfCreation, trunkVolume: trunkVolume)
     }
 }
+
